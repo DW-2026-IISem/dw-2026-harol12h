e2a89acba06aa6ae2ac3b7997dae16504354a6d1
Harol Polo <harolpolomeza@gmail.com>
2026-09-15 10:35:54 -0500

    src/features/business/orders/orders.module.ts

--- documentacion/documentacion.md
+++ documentacion/documentacion.md
@@ -7523,3 +7523,40 @@ export { OrdersModule } from './orders.module';
 EOF_BACKEND_MANUAL
 ```
 ![](img/198.png)
+
+#### 10.2.21 src/features/business/orders/orders.module.ts
+
+Módulo NestJS del feature Orders.
+
+**ruta** `src/features/business/orders/orders.module.ts`
+
+```bash
+cat > src/features/business/orders/orders.module.ts <<'EOF_BACKEND_MANUAL'
+import { Module } from '@nestjs/common';
+import { ORDER_REPOSITORY } from './domain/interfaces/order-repository.interface';
+import { OrderRepository } from './infrastructure/persistence/repositories/order.repository';
+import { CreateOrderUseCase } from './application/use-cases/create-order.use-case';
+import { UpdateOrderUseCase } from './application/use-cases/update-order.use-case';
+import { DeleteOrderUseCase } from './application/use-cases/delete-order.use-case';
+import { GetOrderUseCase } from './application/use-cases/get-order.use-case';
+import { ListOrdersUseCase } from './application/use-cases/list-orders.use-case';
+import { OrdersController } from './presentation/http/controllers/orders.controller';
+
+@Module({
+  controllers: [OrdersController],
+  providers: [
+    OrderRepository,
+    { provide: ORDER_REPOSITORY, useExisting: OrderRepository },
+    CreateOrderUseCase,
+    UpdateOrderUseCase,
+    DeleteOrderUseCase,
+    GetOrderUseCase,
+    ListOrdersUseCase,
+  ],
+  exports: [ORDER_REPOSITORY],
+})
+export class OrdersModule {}
+EOF_BACKEND_MANUAL
+```
+
+![](img/199.png)
--- nestjs/express_sequelize/backend_manual/src/features/business/orders/orders.module.ts
+++ nestjs/express_sequelize/backend_manual/src/features/business/orders/orders.module.ts
@@ -1,21 +1,24 @@
 import { Module } from '@nestjs/common';
-import { OrdersController } from './infrastructure/controllers/orders.controller.js';
-import { OrderRepository } from './infrastructure/persistence/repositories/order.repository.js';
-import { CreateOrderUseCase } from './application/use-cases/create-order.use-case.js';
-import { GetOrderUseCase } from './application/use-cases/get-order.use-case.js';
-import { ListOrdersUseCase } from './application/use-cases/list-orders.use-case.js';
-import { UpdateOrderUseCase } from './application/use-cases/update-order.use-case.js';
-import { DeleteOrderUseCase } from './application/use-cases/delete-order.use-case.js';
+import { ORDER_REPOSITORY } from './domain/interfaces/order-repository.interface';
+import { OrderRepository } from './infrastructure/persistence/repositories/order.repository';
+import { CreateOrderUseCase } from './application/use-cases/create-order.use-case';
+import { UpdateOrderUseCase } from './application/use-cases/update-order.use-case';
+import { DeleteOrderUseCase } from './application/use-cases/delete-order.use-case';
+import { GetOrderUseCase } from './application/use-cases/get-order.use-case';
+import { ListOrdersUseCase } from './application/use-cases/list-orders.use-case';
+import { OrdersController } from './presentation/http/controllers/orders.controller';
 
 @Module({
   controllers: [OrdersController],
   providers: [
     OrderRepository,
+    { provide: ORDER_REPOSITORY, useExisting: OrderRepository },
     CreateOrderUseCase,
-    GetOrderUseCase,
-    ListOrdersUseCase,
     UpdateOrderUseCase,
     DeleteOrderUseCase,
+    GetOrderUseCase,
+    ListOrdersUseCase,
   ],
+  exports: [ORDER_REPOSITORY],
 })
 export class OrdersModule {}
