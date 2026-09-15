bfd0e985e16a583742d0201a2dff467fece0100b
Harol Polo <harolpolomeza@gmail.com>
2026-09-15 10:52:25 -0500

    src/infrastructure/database/seeders/database-seeder.service.ts

--- documentacion/documentacion.md
+++ documentacion/documentacion.md
@@ -7619,3 +7619,30 @@ export class BusinessModule {}
 EOF_BACKEND_MANUAL
 ```
 ![](img/201.png)
+
+#### 10.2.24 src/infrastructure/database/seeders/database-seeder.service.ts
+
+Ejecutar seedOrders.
+
+**ruta** `src/infrastructure/database/seeders/database-seeder.service.ts`
+
+```bash
+cat > src/infrastructure/database/seeders/database-seeder.service.ts <<'EOF_BACKEND_MANUAL'
+import { Injectable } from '@nestjs/common';
+import { seedClients } from '../../../features/business/clients/infrastructure/persistence/seeders/clients.seeder';
+import { seedCollections } from '../../../features/business/collections/infrastructure/persistence/seeders/collections.seeder';
+import { seedProducts } from '../../../features/business/products/infrastructure/persistence/seeders/products.seeder';
+import { seedOrders } from '../../../features/business/orders/infrastructure/persistence/seeders/orders.seeder';
+
+@Injectable()
+export class DatabaseSeederService {
+  async runAllSeeders(): Promise<void> {
+    await seedClients();
+    await seedCollections();
+    await seedProducts();
+    await seedOrders();
+  }
+}
+EOF_BACKEND_MANUAL
+```
+![](img/202.png)
--- nestjs/express_sequelize/backend_manual/src/infrastructure/database/seeders/database-seeder.service.ts
+++ nestjs/express_sequelize/backend_manual/src/infrastructure/database/seeders/database-seeder.service.ts
@@ -1,13 +1,15 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { seedClients } from '../../../features/business/clients/infrastructure/persistence/seeders/clients.seeder.js';
-import { seedCollections } from '../../../features/business/collections/infrastructure/persistence/seeders/collections.seeder.js';
+import { Injectable } from '@nestjs/common';
+import { seedClients } from '../../../features/business/clients/infrastructure/persistence/seeders/clients.seeder';
+import { seedCollections } from '../../../features/business/collections/infrastructure/persistence/seeders/collections.seeder';
+import { seedProducts } from '../../../features/business/products/infrastructure/persistence/seeders/products.seeder';
+import { seedOrders } from '../../../features/business/orders/infrastructure/persistence/seeders/orders.seeder';
 
 @Injectable()
-export class DatabaseSeederService implements OnModuleInit {
-  async onModuleInit(): Promise<void> {
-    if (process.env.NODE_ENV === 'production') return;
-
+export class DatabaseSeederService {
+  async runAllSeeders(): Promise<void> {
     await seedClients();
     await seedCollections();
+    await seedProducts();
+    await seedOrders();
   }
 }
