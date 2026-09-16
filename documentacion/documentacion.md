3d73438f5396bfd2db22f2ed4023cfbf4eca2659
Harol Polo <harolpolomeza@gmail.com>
2026-09-15 23:37:53 -0500

    #### src/infrastructure/database/seeders/database-seeder.service.ts

--- documentacion/documentacion.md
+++ documentacion/documentacion.md
@@ -9469,3 +9469,35 @@ export class BranchesModule {}
 EOF_BACKEND_MANUAL
 ```
 ![](img/256.png)
+
+#### 
+
+```baash 
+import { Module } from '@nestjs/common';
+import { ClientsModule } from './clients/clients.module.js';
+import { CollectionsModule } from './collections/collections.module.js';
+import { ProductsModule } from './products/products.module.js';
+import { OrdersModule } from './orders/orders.module.js';
+import { OrderDetailsModule } from './order-details/order-details.module.js';
+import { VariantsModule } from './variants/variants.module.js';
+
+@Module({
+  imports: [
+    ClientsModule,
+    CollectionsModule,
+    ProductsModule,
+    OrdersModule,
+    OrderDetailsModule,
+    VariantsModule,
+  ],
+  exports: [
+    ClientsModule,
+    CollectionsModule,
+    ProductsModule,
+    OrdersModule,
+    OrderDetailsModule,
+    VariantsModule,
+  ],
+})
+export class BusinessModule {} 
+```
--- nestjs/express_sequelize/backend_manual/src/infrastructure/database/sequelize/sequelize.factory.ts
+++ nestjs/express_sequelize/backend_manual/src/infrastructure/database/sequelize/sequelize.factory.ts
@@ -5,6 +5,7 @@ import { ProductModel } from '../../../features/business/products/infrastructure
 import { OrderModel } from '../../../features/business/orders/infrastructure/persistence/models/order.model.js';
 import { OrderDetailModel } from '../../../features/business/order-details/infrastructure/persistence/models/order-detail.model.js';
 import { VariantModel } from '../../../features/business/variants/infrastructure/persistence/models/variant.model.js';
+import { BranchModel } from '../../../features/business/branches/infrastructure/persistence/models/branch.model.js';
 
 export const ALL_MODELS = [
   ClientModel,
@@ -13,6 +14,7 @@ export const ALL_MODELS = [
   OrderModel,
   OrderDetailModel,
   VariantModel,
+  BranchModel
 ];
 
 export async function createSequelizeInstance(options: any): Promise<Sequelize> {
@@ -33,6 +35,9 @@ export async function createSequelizeInstance(options: any): Promise<Sequelize> 
 
   ProductModel.hasMany(OrderDetailModel, { foreignKey: 'productId' });
   OrderDetailModel.belongsTo(ProductModel, { foreignKey: 'productId' });
+
+  BranchModel.hasMany(ClientModel, { foreignKey: 'branchId' });
+  ClientModel.belongsTo(BranchModel, { foreignKey: 'branchId' });
 
   return sequelize;
 }
